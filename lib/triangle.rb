839509e43a163a655719a26dea6f51743bf26dae
Christopher Luna <chris@illusionlayervfx.com>
2019-09-05 03:25:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :angle1, :angle2, :angle3
+
+  def initialize(angle1, angle2, angle3)
+    @angle1 = angle1
+    @angle2 = angle2
+    @angle3 = angle3
+  end
+
+  def kind
+    if self.angle1 == self.angle3 && self.angle2 == self.angle3
+      :equilateral
+    elsif self.angle1 || self.angle2 == self.angle3
+      :isosceles
+    elsif self.angle1 != (self.angle2 != self.angle3) && self.angle2 != self.angle3
+      :scalene
+    else
+
+    end
+
+
+  end
+
+    #class TriangleError < StandardError
+
+    #end
+
 end
